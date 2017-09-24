d2ff32da536837d47c56089ab99a7d3796f758a3
eda013 <poukupenga@gmail.com>
2017-09-24 15:38:00 +1300

    hey im setting it up from scratch mostly

---
+++ routes.js
@@ -0,0 +1,9 @@
+
+const express = require('express')
+const router = express.Router()
+const data = require('./data')
+//const fs = require('fs')
+
+//routes!!
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+//Malcom-in-the-Middleware
+app.engine('hbs', hbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use('/', routes)
+
+module.exports = app
