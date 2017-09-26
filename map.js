0c3636573af245c233c808224daa70109b589b53
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-09-26 18:26:26 +1300

    added gmaps map woo

---
+++ map.js
@@ -0,0 +1,12 @@
+var map = new google.maps.Map(document.getElementById.Id('map'), options)
+
+function initMap () {
+  var options = {
+    zoom: 10,
+    center: {
+      lat: -41.2865,
+      lng: 174.7762
+    }
+  }
+  var map = new google.maps.Map(document.getElementById('map'), options)
+}
