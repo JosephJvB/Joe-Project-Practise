ee38c44c1027a7b0515e3e52b906a5d4c0da5cdb
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-09-30 18:48:54 +1300

    adding pages im slow and a route im dumb lol

--- routes.js
+++ routes.js
@@ -2,16 +2,23 @@
 const express = require('express')
 const router = express.Router()
 const data = require('./data')
-//const fs = require('fs') wont need till read/writefile
+// const fs = require('fs') wont need till read/writefile
 
-//routes!!
-router.get('/', (req, res) => {
-  res.render('')
-})
+// routes!!
 
 router.get('/skateApp', (req, res) => {
-res.render('map', data)
+  res.render('home', data)
 })
 
+router.get('/skateApp/spots/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const thatSpot = data.spots.find((spot) => {
+    return spot.id === id
+  })
+  res.render('spot', thatSpot)
+})
 
+router.get('/', (req, res) => {
+  res.send('<a href="/skateApp">butts</a>')
+})
 module.exports = router
